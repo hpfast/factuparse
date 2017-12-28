a8863e89bebf8798f9b665425593ca8bd8a1b590
Hans Fast <hans@surfacemarkup.net>
2017-12-28 21:10:50 +0100

    initial commit

---
+++ cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+let acctParser = require('./index');
+acctParser();
---
+++ index.js
@@ -0,0 +1,46 @@
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+let inputfile = '';
+if (process.argv.length === 3) {
+  inputfile = process.argv[2];
+} else {
+  console.log('usage: factup INPUT_FILE > OUTPUT_FILE')
+}
+
+const input = fs.readFileSync(inputfile, 'utf-8');
+const table = parse(input, {columns: true});
+
+function selectAccount(record){
+  if (record.Indienwijze === 'Vecozo') {
+    record.Account = 'Zorgverzekeraars'
+  } else {
+    record.Account = 'Zelfbetalers'
+  }
+  return record;
+}
+
+function formatDate(record) {
+  record.Datum = record.Datum.split('-').reverse().join('/');
+  return record;
+}
+
+function formatNumber(record) {
+  const re = /,/;
+  record.Bedrag = record.Bedrag.replace(re, '.');
+  return record;
+}
+
+function formatRecord(record) {
+  return `${record.Datum} (#${record.Nummer}) ${record.Debiteur}
+    Assets:Receivable:${record.Account}    ${record.Bedrag}
+    Income:behandeling:${record.Account}`
+
+}
+
+
+function main() {
+  const tablewithaccount = table.map(selectAccount).map(formatDate).map(formatNumber).map(formatRecord)
+  console.log(tablewithaccount.join('\n\n'))
+}
+
+module.exports = main;
